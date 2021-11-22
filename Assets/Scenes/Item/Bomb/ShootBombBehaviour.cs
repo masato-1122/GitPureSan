d05968835d1fcdfdd2fc8ba9525d62a4fb89ad4b
masato-1122 <70794504+masato-1122@users.noreply.github.com>
2021-11-22 14:23:21 +0900

    ステージ編集

--- Assets/Scenes/Item/Bomb/ShootBombBehaviour.cs
+++ Assets/Scenes/Item/Bomb/ShootBombBehaviour.cs
@@ -13,6 +13,11 @@ public class ShootBombBehaviour : MonoBehaviour
     void Start()
     {
         timer = 0;
+        Vector3 forceDirection = new Vector3(1.0f, 1.0f, 0f);
+        float forceMagnitude = 10.0f;
+        Vector3 force = forceMagnitude * forceDirection;
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(force, ForceMode.Impulse);
     }
 
     // Update is called once per frame
