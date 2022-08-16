1c18452c80c26b0afd5a372118f5900ebabbe4fa
Your Name <you@example.com>
2022-08-17 00:10:01 +0900

    迷路壁修正

--- Assets/Scenes/Facility/Maze/ReadingMaze.cs
+++ Assets/Scenes/Facility/Maze/ReadingMaze.cs
@@ -15,13 +15,18 @@ public class ReadingMaze : MonoBehaviour
     public GameObject start;
     public GameObject goal;
 
+    public GameObject wall1;
+    public GameObject wall2;
+    public GameObject wall3;
+    public GameObject wall4;
+
     const int WALL = 1;
     const int PATH = 0;
 
     private int[,] field;
     private string[] textData;
 
-
+    //
     // Start is called before the first frame update
     void Start()
     {
@@ -40,7 +45,6 @@ public class ReadingMaze : MonoBehaviour
             for( int j = 0; j < mazeSize; j++)
             {
                 field[i, j] = int.Parse(csvDatas[i][j]);
-                Debug.Log(field[i, j]);
                 
                 if(int.Parse(csvDatas[i][j]) == 1)
                 {
@@ -55,6 +59,9 @@ public class ReadingMaze : MonoBehaviour
         start.transform.position = new Vector3(1*3+40, 0, 1*3+400);
         goal.transform.position = new Vector3((mazeSize-2)*3+40, 1, (mazeSize-2)*3+400);
 
-
+        wall1.transform.position = new Vector3((start.transform.position.x + goal.transform.position.x)/2, start.transform.position.y, goal.transform.position.z + 3);
+        wall2.transform.position = new Vector3((start.transform.position.x + goal.transform.position.x)/2, start.transform.position.y, start.transform.position.z - 3);
+        wall3.transform.position = new Vector3(goal.transform.position.x + 3, start.transform.position.y, (start.transform.position.z + goal.transform.position.z)/2);
+        wall4.transform.position = new Vector3(start.transform.position.x - 3, start.transform.position.y, (start.transform.position.z + goal.transform.position.z)/2);
     }
 }
